5fc03cddc3c542509f9179fe4b969001093e4615
Njosephine <josephinenanyonga389@gmail.com>
2026-03-11 15:09:01 +0300

    Add event and article changes

--- src/api/event/routes/event.ts
+++ src/api/event/routes/event.ts
@@ -4,4 +4,13 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::event.event');
+export default factories.createCoreRouter('api::event.event', {
+  config: {
+    find: {
+      auth: false,
+    },
+    findOne: {
+      auth: false,
+    },
+  },
+});
--- src/api/news-article/routes/news-article.ts
+++ src/api/news-article/routes/news-article.ts
@@ -4,4 +4,13 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::news-article.news-article');
+export default factories.createCoreRouter('api::news-article.news-article', {
+  config: {
+    find: {
+      auth: false,
+    },
+    findOne: {
+      auth: false,
+    },
+  },
+});
